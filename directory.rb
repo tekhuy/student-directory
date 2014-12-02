b889ae5a83e99206ff5096e6da45e4a4513ac144
tekhuy <huyle20@hotmail.com>
2014-12-02 23:20:37 +0000

    director.rb v6

--- directory.rb
+++ directory.rb
@@ -1,37 +1,20 @@
-student_count = 30
-
-students = [
-{:name => "Sanda Golcea", :cohort => :December},
-{:name => "Matteo Manzo", :cohort => :December},
-{:name => "Bibiana Cristòfol", :cohort => :December},
-{:name => "Steph Oldcorn", :cohort => :December},
-{:name => "Huy Le", :cohort => :December},
-{:name => "Kieran Goodacre", :cohort => :December},
-{:name => "Emily Sas", :cohort => :December},
-{:name => "India Dearlove", :cohort => :December},
-{:name => "Jacob Mitchinson", :cohort => :December},
-{:name => "Marcin Wal", :cohort => :December},
-{:name => "Andy Newman", :cohort => :December},
-{:name => "Richard Ighodaro", :cohort => :December},
-{:name => "Ptolemy Barnes", :cohort => :December},
-{:name => "Marcus de Wilde", :cohort => :December},
-{:name => "Bebbs", :cohort => :December},
-{:name => "Luke", :cohort => :December},
-{:name => "Oliver Lucas", :cohort => :December},
-{:name => "Jack Rubio", :cohort => :December},
-{:name => "David Blake", :cohort => :December},
-{:name => "Charlie Walsh", :cohort => :December},
-{:name => "Danielle", :cohort => :December},
-{:name => "Gabe", :cohort => :December},
-{:name => "Jin Dai", :cohort => :December},
-{:name => "Gus Powell", :cohort => :December},
-{:name => "Jake Alvarez", :cohort => :December},
-{:name => "Hannah Carney", :cohort => :December},
-{:name => "Clint", :cohort => :December},
-{:name => "Michael Colley", :cohort => :December},
-{:name => "JJ Lakin", :cohort => :December},
-{:name => "Will", :cohort => :December}
-]
+def input_students
+	puts "Please enter the ames of the students"
+	puts "to finish, just hit return twice"
+	#create an empty array
+	students = []
+	#get the first name
+	name = gets.chomp
+	#while the name is not empty, repeat this code
+	while !name.empty? do
+		#add the student hash to the array
+		students << {:name => name, :cohort => :December}
+		#get another name from the user
+		name = gets .chomp
+	end
+	#return the array of students
+	students
+end
 
 def print_header
 	puts "Makers Academy 8th December 2014 Cohort"
@@ -48,6 +31,7 @@ def print_footer(names)
 	puts "Overall, we have #{names.length} great students."
 end
 
+students = input_students
 print_header
 print(students)
 print_footer(students)
