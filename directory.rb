2fc0a056351fc269fdde88ffe82ae5ef8b2aa3eb
tekhuy <huyle20@hotmail.com>
2014-12-02 23:08:05 +0000

    First hash for director.rb

--- directory.rb
+++ directory.rb
@@ -1,36 +1,36 @@
 student_count = 30
 
 students = [
-"Sanda Golcea",
-"Matteo Manzo",
-"Bibiana Cristòfol",
-"Steph Oldcorn",
-"Huy Le",
-"Kieran Goodacre",
-"Emily Sas", 
-"India Dearlove",
-"Jacob Mitchinson", 
-"Marcin Wal",
-"Andy Newman",
-"Richard Ighodaro",
-"Ptolemy Barnes",
-"Marcus de Wilde",
-"Bebbs",
-"Luke",
-"Oliver Lucas",
-"Jack Rubio",
-"David Blake",
-"Charlie Walsh",
-"Danielle",
-"Gabe",
-"Jin Dai",
-"Gus Powell",
-"Jake Alvarez",
-"Hannah Carney",
-"Clint",
-"Michael Colley",
-"JJ Lakin",
-"Will",
+{:name => "Sanda Golcea", :cohort => :December},
+{:name => "Matteo Manzo", :cohort => :December},
+{:name => "Bibiana Cristòfol", :cohort => :December},
+{:name => "Steph Oldcorn", :cohort => :December},
+{:name => "Huy Le", :cohort => :December},
+{:name => "Kieran Goodacre", :cohort => :December},
+{:name => "Emily Sas", :cohort => :December},
+{:name => "India Dearlove", :cohort => :December},
+{:name => "Jacob Mitchinson", :cohort => :December},
+{:name => "Marcin Wal", :cohort => :December},
+{:name => "Andy Newman", :cohort => :December},
+{:name => "Richard Ighodaro", :cohort => :December},
+{:name => "Ptolemy Barnes", :cohort => :December},
+{:name => "Marcus de Wilde", :cohort => :December},
+{:name => "Bebbs", :cohort => :December},
+{:name => "Luke", :cohort => :December},
+{:name => "Oliver Lucas", :cohort => :December},
+{:name => "Jack Rubio", :cohort => :December},
+{:name => "David Blake", :cohort => :December},
+{:name => "Charlie Walsh", :cohort => :December},
+{:name => "Danielle", :cohort => :December},
+{:name => "Gabe", :cohort => :December},
+{:name => "Jin Dai", :cohort => :December},
+{:name => "Gus Powell", :cohort => :December},
+{:name => "Jake Alvarez", :cohort => :December},
+{:name => "Hannah Carney", :cohort => :December},
+{:name => "Clint", :cohort => :December},
+{:name => "Michael Colley", :cohort => :December},
+{:name => "JJ Lakin", :cohort => :December},
+{:name => "Will", :cohort => :December}
 ]
 
 def print_header
@@ -38,9 +38,9 @@ def print_header
 	puts "---------------------------------------"
 end
 
-def print(names)
-	names.each do |name|
-		puts name
+def print(students)
+	students.each do |student|
+		puts "#{student[:name]} (#{student[:cohort]} cohort)"
 	end
 end
 
