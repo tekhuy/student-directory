2ff40ed15ad7f329d0992ec943b150ca3f010bc2
tekhuy <huyle20@hotmail.com>
2014-12-02 16:22:18 +0000

    Added a list of students

---
+++ directory.rb
@@ -0,0 +1,8 @@
+puts "Makers Academy 8th December 2014 Cohort"
+puts "---------------------------------------"
+
+puts "Sanda Golcea"
+puts "Matteo Manzo"
+puts "Bibiana "
+puts "Steph Oldcorn"
+puts "Huy Le"
