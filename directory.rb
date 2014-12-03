8a8729c915009caa464c9e8c5286959e838cfa68
tekhuy <huyle20@hotmail.com>
2014-12-03 17:17:52 +0000

    directory.rb v9 - Loading data from file

--- directory.rb
+++ directory.rb
@@ -12,6 +12,15 @@ def save_students
   file.close
 end
 
+def load_students
+  file = File.open("students.csv", "r")
+  file.readlines.each do |line|
+    name, cohort = line.chomp.split(',')
+    @students << {:name => name, :cohort => cohort.to_sym}
+  end
+  file.close
+end
+
 def print_header
   puts "The students of my cohort at Makers Academy"
   puts "-------------"
@@ -46,6 +55,7 @@ def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
   puts "3. Save the list to students.csv"
+  puts "4. Load the list from students.csv"
   puts "9. Exit" # 9 because we'll be adding more items  
 end
 
@@ -63,6 +73,8 @@ def process(selection)
     show_students
   when "3"
     save_students
+  when "4"
+    load_students
   when "9"
     exit
   else
